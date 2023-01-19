a2d84fe99175140b678f4bad94b84e2a29078f31
Guilherme Souza <guilherme_souza@mddweb.com.br>
2023-01-19 15:56:13 -0300

    feat: Light Theme configurado

--- crud/src/routes/index.tsx
+++ crud/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='pagina-inicial' element={<p> Página inicial</p>} />
+            <Route path='pagina-inicial' 
+            element={<Button color='primary' variant='contained'>Button</Button>} />
 
-            <Route path='*' element={<Navigate to='pagina-inicial' />}/>
+            <Route path='*' element={<Navigate to='pagina-inicial' />} />
         </Routes>
     );
 }
