a0a90f58e9f95f5317c16d54c1074f448510328c
Guilherme Souza <guilherme_souza@mddweb.com.br>
2023-01-19 17:13:03 -0300

    feat: Theme Dark foi configurado

--- crud/src/routes/index.tsx
+++ crud/src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 
+import { UseAppThemeContext } from '../shared/contexts';
+
 export const AppRoutes = () => {
+    const { toggleTheme } = UseAppThemeContext();
+
     return (
         <Routes>
-            <Route path='pagina-inicial' 
-            element={<Button color='primary' variant='contained'>Button</Button>} />
+            <Route path='pagina-inicial'
+                element={<Button color='primary' variant='contained' onClick={toggleTheme}>Toggle Theme</Button>} />
 
             <Route path='*' element={<Navigate to='pagina-inicial' />} />
         </Routes>
---
+++ crud/src/shared/themes/Dark.ts
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material';
+import { indigo, yellow } from '@mui/material/colors';
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary: {
+            main: indigo[700],
+            dark: indigo[800],
+            light: indigo[500],
+            contrastText: '#FFFFFF'
+        },
+        secondary: {
+            main: yellow[700],
+            dark: yellow[800],
+            light: yellow[500],
+            contrastText: '#FFFFFF'
+        },
+        background: {
+            default: '#0a1929',
+            paper: '#001e3c '
+        }
+    },
+});
