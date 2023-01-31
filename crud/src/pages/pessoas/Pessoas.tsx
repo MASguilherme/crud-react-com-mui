44fbc7514ff70b35a03a29fb2f6ce5bb92ab0051
Guilherme <guilherme.amsouza@gmail.com>
2023-01-30 21:52:53 -0300

    feat: search Params configurado

--- crud/src/pages/pessoas/Pessoas.tsx
+++ crud/src/pages/pessoas/Pessoas.tsx
@@ -1,13 +1,30 @@
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 import { FerramentaDeListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Pessoas: React.FC = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+
   return (
-    <LayoutBaseDePagina titulo='Listagem de Pessoas'>
-      <FerramentaDeListagem
-        textoBotaoNovo='Nova'
-        mostrarInputDeBusca
-      />
+    <LayoutBaseDePagina
+      titulo='Listagem de Pessoas'
+      barraDeFerramentas={
+        <FerramentaDeListagem
+          mostrarInputDeBusca
+          textoDeBusca={busca}
+          textoBotaoNovo='Nova'
+          aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+        />
+      }
+    >
+
     </LayoutBaseDePagina>
   );
 };
